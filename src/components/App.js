541e121ba8325e933627c1a95ddc3c5ed9bb42bd
KotKamil <62114754+KotKamil@users.noreply.github.com>
2022-11-27 02:21:33 +0100

    Added randomized cards

--- src/components/App.js
+++ src/components/App.js
@@ -9,43 +9,78 @@ class App extends React.Component {
     cards: [
       {
         id: 0,
-        value: 0,
+        value: null,
         isFaceUp: false,
         isPaired: false
       },
       {
         id: 1,
-        value: 2,
+        value: null,
         isFaceUp: false,
         isPaired: false
       },
       {
         id: 2,
-        value: 0,
+        value: null,
         isFaceUp: false,
         isPaired: false
       },
       {
         id: 3,
-        value: 2,
+        value: null,
         isFaceUp: false,
         isPaired: false
       },
       {
         id: 4,
-        value: 3,
+        value: null,
         isFaceUp: false,
         isPaired: false
       },
       {
         id: 5,
-        value: 3,
+        value: null,
         isFaceUp: false,
         isPaired: false
       },
     ],
     cardsTurned: 0,
     gameState: "PLAYING"
+  }
+
+  initGame = () => {
+    const cards = this.state.cards.map(item => item);
+    const cardValues = [];
+    function shuffle(array) {
+      let currentIndex = array.length, randomIndex;
+
+      // While there remain elements to shuffle.
+      while (currentIndex != 0) {
+
+        // Pick a remaining element.
+        randomIndex = Math.floor(Math.random() * currentIndex);
+        currentIndex--;
+
+        // And swap it with the current element.
+        [array[currentIndex], array[randomIndex]] = [
+          array[randomIndex], array[currentIndex]];
+      }
+
+      return array;
+    }
+
+    for (let i = 0; i < 3; i++) {
+      const randomNumber = Math.floor(Math.random() * 10);
+      cardValues[i] = randomNumber;
+      cardValues[i + 3] = randomNumber;
+    }
+    shuffle(cardValues);
+
+    cards.forEach((card, index) => {
+      card.value = cardValues[index];
+    });
+
+    this.setState(prevState => ({ cards }));
   }
 
   checkForPair = (cards) => {
@@ -98,7 +133,7 @@ class App extends React.Component {
 
     if (cardsTurned === 2) {
       if (this.checkForPair(cards)) cardsTurned = 0;
-      else setTimeout(this.turnCards, 2000);
+      else setTimeout(this.turnCards, 500);
     }
 
     this.setState(prevState => ({ cards, cardsTurned }));
@@ -117,6 +152,10 @@ class App extends React.Component {
     cardsTurned = 0;
 
     this.setState(prevState => ({ cards, cardsTurned }));
+  }
+
+  componentDidMount() {
+    this.initGame();
   }
 
   render() {
